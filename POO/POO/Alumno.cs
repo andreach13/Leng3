faa6fa97da34a5ea1545240ee3cf5e64a01dd5d7
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-02-10 23:27:24 -0600

    agregando ejercicios de clase

---
+++ POO/POO/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Direccion Direccion { get; set; }
+
+        public Alumno()
+        {
+            Direccion = new Direccion();
+        }
+    }
+}
---
+++ POO/POO/Direccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Direccion
+    {
+        public string Calle { get; set; }
+        public string Avenida { get; set; }
+        public string Barrio0Colonia { get; set; }
+    }
+}
