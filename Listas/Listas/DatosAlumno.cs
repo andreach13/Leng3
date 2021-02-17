64cbd1caca59d4efb133a6abdc4699729d24e88c
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-02-16 18:36:53 -0600

    Agregando ejercicios de clase

---
+++ Listas/Listas/DatosAlumno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    public class DatosAlumno
+    {
+        //public List<int> Numeros { get; set; }
+        //public List<string> Nombres { get; set; }
+        public List<Alumno> Alumnos { get; set; }
+
+        public DatosAlumno()
+        {
+            //Numeros = new List<int>();
+            // Numeros.Add(10);
+            //Numeros.Add(5);
+
+            //Nombres = new List<string>();
+            //Nombres.Add("Juan");
+            //Nombres.Add("Maria");
+
+            //Alumno a = new Alumno();
+            //a.Nombre = "Juan";
+            //Alumno b = new Alumno();
+            //b.Nombre = "Maria";
+
+            Alumnos = new List<Alumno>();
+            //Alumnos.Add(a);
+            //Alumnos.Add(b);
+        }
+
+        public void Agregar(Alumno nuevoAlumno)
+        {
+            Alumnos.Add(nuevoAlumno);
+        }
+
+        //Alumnos.Sort -- ordenar
+        //Alumnos.Count -- contar
+        //Alumnos.Find -- encontrar
+    }
+}
---
+++ Listas/Listas/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listas
+{
+    public partial class Form1 : Form
+    {
+        DatosAlumno _datos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _datos = new DatosAlumno();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Alumno a = new Alumno();
+            a.Nombre = textBox1.Text;
+            a.Telefono = textBox2.Text;
+            a.Direccion = textBox3.Text;
+
+            _datos.Agregar(a);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = _datos.Alumnos;
+
+            comboBox1.DataSource = null;
+            comboBox1.DataSource = _datos.Alumnos;
+            comboBox1.DisplayMember = "Nombre";
+
+            listBox1.DataSource = null;
+            listBox1.DataSource = _datos.Alumnos;
+            listBox1.DisplayMember = "Nombre";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
